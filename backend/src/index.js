024bec3f7ed63bb0e800003fadd43dc55d80c58d
Yash Pandey <yashdevesh10@gmail.com>
2024-12-16 20:32:59 +0530

    Added Delete button funcationality

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,8 +8,10 @@ require('dotenv').config();
 
 // const Mongo_URL = "mongodb://127.0.0.1:27017/task-manager-project";
 // const PORT = 8082;
+const Mongo_URL = process.env.MONGODB_URL;
+const PORT = process.env.PORT || 10000;
 
-mongoose.connect(process.env.Mongo_URL)
+mongoose.connect(Mongo_URL)
 .then(() => {
     console.log("MongoDB connected successfully");
 }).catch((err) => {
@@ -20,6 +22,6 @@ app.use(express.json());
 app.use("/users", userRouter);
 app.use("/tasks", taskRouter);
 
-app.listen(process.env.PORT, () => {
-    console.log(`Backend is running on Port : ${process.env.PORT}`);
+app.listen(PORT, () => {
+    console.log(`Backend is running on Port : ${PORT}`);
 })
