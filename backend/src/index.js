f7c02cf583a4f60407a60c47b12bc3bb858f08b2
Yash Pandey <yashdevesh10@gmail.com>
2024-12-16 20:11:38 +0530

    adding in .env file

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,10 @@ const userRouter = require("./routes/user.routers");
 const cors = require("cors");
 require('dotenv').config();
 
-const Mongo_URL = "mongodb://127.0.0.1:27017/task-manager-project";
-const PORT = 8082;
+// const Mongo_URL = "mongodb://127.0.0.1:27017/task-manager-project";
+// const PORT = 8082;
 
-mongoose.connect(Mongo_URL)
+mongoose.connect(process.env.Mongo_URL)
 .then(() => {
     console.log("MongoDB connected successfully");
 }).catch((err) => {
@@ -20,6 +20,6 @@ app.use(express.json());
 app.use("/users", userRouter);
 app.use("/tasks", taskRouter);
 
-app.listen(PORT, () => {
-    console.log(`Backend is running on Port : ${PORT}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Backend is running on Port : ${process.env.PORT}`);
 })
